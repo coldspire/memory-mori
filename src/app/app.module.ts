354973c3c4ad31715dca8fdaff53b6d9aa5e3881
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2020-09-05 22:11:48 -0400

    Add/integrate mori board and card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardModule } from './board/board.module';
-import { CardModule } from './card/card.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BoardModule, CardModule],
+  imports: [BrowserModule, AppRoutingModule, BoardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/board/board/board.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mori-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
